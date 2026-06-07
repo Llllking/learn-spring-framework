294ceca2d59552049d0975f9f5d85a85d519d9d5
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-06-07 23:50:39 +0100

    add AppGamingSpringBoot with its Configuration class AppGamingConfiguration

--- src/main/java/com/ismail/learn_spring_framework/AppGamingSpringBoot.java
+++ src/main/java/com/ismail/learn_spring_framework/AppGamingSpringBoot.java
@@ -1,4 +1,17 @@
 package com.ismail.learn_spring_framework;
 
+import com.ismail.learn_spring_framework.game.GameRunner;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AppGamingSpringBoot {
+    static void main(String[] args) {
+        try (var context =
+                     new AnnotationConfigApplicationContext
+                             (AppGamingConfiguration.class)){
+            context.getBean(GameRunner.class).run();
+        }
+
+
+    }
+
 }
