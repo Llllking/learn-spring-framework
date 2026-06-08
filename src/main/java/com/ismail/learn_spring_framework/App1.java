08055630920cd925cbb5cf9075d1813e8e57423d
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-06-08 20:04:32 +0100

    crete dependency injection types starter

---
+++ src/main/java/com/ismail/learn_spring_framework/App1.java
@@ -0,0 +1,39 @@
+package com.ismail.learn_spring_framework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+class myBusinessClass {
+    private Dependency1 dependency1;
+    private Dependency2 dependency2;
+
+    @Override
+    public String toString(){
+        return dependency1 + " " + dependency2;
+    }
+}
+
+@Component
+class Dependency1 {}
+
+@Component
+class Dependency2 {}
+
+@Configuration
+@ComponentScan
+public class App1 {
+
+    static void main(String[] args) {
+        try (var context =
+                     new AnnotationConfigApplicationContext
+                             (App1.class)){
+            System.out.println(context.getBean(myBusinessClass.class));
+        }
+
+
+    }
+
+}
