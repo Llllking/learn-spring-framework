65c2bc9f83aec3b5fe6dfce5f2be9d95589b7101
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-06-08 20:12:17 +0100

    setter injection

--- src/main/java/com/ismail/learn_spring_framework/App1.java
+++ src/main/java/com/ismail/learn_spring_framework/App1.java
@@ -8,10 +8,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 class myBusinessClass {
-    @Autowired
     private Dependency1 dependency1;
-    @Autowired
     private Dependency2 dependency2;
+
+    @Autowired
+    public void setDependency1(Dependency1 dependency1) {
+        this.dependency1 = dependency1;
+    }
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        this.dependency2 = dependency2;
+    }
 
     @Override
     public String toString(){
